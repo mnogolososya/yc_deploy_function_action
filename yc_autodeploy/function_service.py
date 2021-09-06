664591ec7d3234247992a2826b43494477441bf6
denis <schmelev94@gmail.com>
2021-09-06 10:46:34 +0300

    skipping no files from src while deploying to YC

--- yc_autodeploy/function_service.py
+++ yc_autodeploy/function_service.py
@@ -125,7 +125,6 @@ class YandexCloudServerlessFunctionService:
 
         with zipfile.ZipFile(buffer, "w", compression=zipfile.ZIP_DEFLATED) as zf:
             for file in pathlib.Path(source_dir).rglob('*'):
-                if file.name not in [settings.DEPLOY_CONFIG_FILE_NAME, '__init__.py']:
-                    zf.write(file, file.name)
+                zf.write(file, file.name)
 
         return buffer.getvalue()
